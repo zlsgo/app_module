7f99e1478703251a6f7cdc12c3ead17313ff8561
seekwe <seekwe@gmail.com>
2024-03-07 19:18:23 +0800

    ✨ feat: 更新模型注册逻辑

--- restapi/restapi.go
+++ restapi/restapi.go
@@ -22,12 +22,19 @@ func New(o ...func(*Options)) (p *Module) {
 		ModuleLifeCycle: service.ModuleLifeCycle{
 			OnDone: func(di zdi.Invoker) error {
 				return di.InvokeWithErrorOnly(func(db *zdb.DB) error {
-					mods, err := NewModels(opt.ModelsDefine, db, true)
-					if err != nil {
-						return err
+
+					mod := NewModels(NewSQL(db), func(o *ModelsOptions) {
+						o.Prefix = "model_"
+					})
+					for _, d := range opt.ModelsDefine {
+						_, err := mod.Reg(d.Name, d, false)
+						if err != nil {
+							return err
+						}
 					}
-					_ = mods
-					// di.(zdi.TypeMapper).Map(mods)
+
+					_ = di.(zdi.TypeMapper).Map(mod)
+
 					return nil
 				})
 			},
