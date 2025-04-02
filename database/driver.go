c0e8ec66da2ef9c746cece45bfcaadb56a083458
sohaha <seekwe@gmail.com>
2025-04-02 17:35:28 +0800

    ✨ feat: 添加数据库驱动注册功能

--- database/driver.go
+++ database/driver.go
@@ -1,7 +1,17 @@
 package database
 
 import (
+	"errors"
+
 	"github.com/zlsgo/zdb/driver"
 )
 
 var drivers = map[string]func(Options) (driver.IfeConfig, error){}
+
+func Register(name string, driver func(Options) (driver.IfeConfig, error)) (err error) {
+	if _, ok := drivers[name]; ok {
+		err = errors.New("数据库驱动[" + name + "]已注册")
+	}
+	drivers[name] = driver
+	return
+}
