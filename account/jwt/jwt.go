1d017c9526c6af1c8e0daf93c3191eb8458b512d
seekwe <seekwe@gmail.com>
2025-07-10 14:18:52 +1000

    ⚙️ refactor: 调整日志记录

--- account/jwt/jwt.go
+++ account/jwt/jwt.go
@@ -15,12 +15,12 @@ type Info struct {
 	jwt.StandardClaims
 	Info      string
 	IsRefresh bool
+	JTI       string
 }
 
 func GenToken(info string, key string, expire, refreshExpire int64) (accessToken, refreshToken string, err error) {
 	if expire == 0 {
-		// 默认过期时间为 24 小时
-		expire = 86400
+		expire = 86400 // 默认过期时间为 24 小时
 	}
 	expiresAt := time.Now().Add(time.Duration(expire) * time.Second).Unix()
 	claims := Info{
@@ -35,12 +35,12 @@ func GenToken(info string, key string, expire, refreshExpire int64) (accessToken
 	}
 
 	if refreshExpire == 0 || refreshExpire < expiresAt {
-		// 设置 refresh token 的过期时间为 7 天后
-		refreshExpire = 604800 + expiresAt
+		refreshExpire = 604800 + expiresAt // refresh token 的默认过期时间为 7 天后
 	}
 
 	claims.StandardClaims.ExpiresAt = refreshExpire
 	claims.IsRefresh = true
+	claims.JTI = zstring.Rand(16)
 	if refreshToken, err = jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(zstring.String2Bytes(key)); err != nil {
 		return "", "", zerror.With(err, "failed to generate refresh token signature")
 	}
@@ -50,6 +50,9 @@ func GenToken(info string, key string, expire, refreshExpire int64) (accessToken
 
 func Parse(token string, tokenKey string) (*Info, error) {
 	t, err := jwt.ParseWithClaims(token, &Info{}, func(token *jwt.Token) (i interface{}, err error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
 		return zstring.String2Bytes(tokenKey), nil
 	})
 	if err != nil {
@@ -57,6 +60,10 @@ func Parse(token string, tokenKey string) (*Info, error) {
 	}
 
 	if claims, ok := t.Claims.(*Info); ok && t.Valid {
+		now := time.Now().Unix()
+		if claims.ExpiresAt < now {
+			return nil, errors.New("token expired")
+		}
 		return claims, nil
 	}
 
